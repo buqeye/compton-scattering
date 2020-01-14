61f00c61aeb6e9cb90fc252476656f671b50d763
Jordan Melendez <jordan.melendez@datarobot.com>
2020-01-14 17:40:39 -0500

    New functions

--- compton/__init__.py
+++ compton/__init__.py
@@ -4,8 +4,9 @@ from .kinematics import omega_cm_from_lab, omega_outgoing_lab, theta_cm_from_lab
     momentum_transfer_cm, momentum_transfer_lab, beta_boost, gamma_boost, dsg_proton_low_energy, sigma3_low_energy
 from .constants import mass_proton, alpha_fine, hbarc, fm2_to_nb, proton_pol_vec_mean, neutron_pol_vec_mean, \
     proton_pol_vec_std, neutron_pol_vec_std, mass_pion, mass_neutron, \
-    proton_pol_vec_trans_mean, proton_pol_vec_trans_std
+    proton_pol_vec_trans_mean, proton_pol_vec_trans_std, neutron_pol_vec_trans_mean, neutron_pol_vec_trans_std, \
+    P_trans
 from .graphs import plot_subsets, plot_utilities_all_observables, plot_comparison_subsets_and_truncation, \
-    plot_comparison_subsets_for_observables
+    plot_comparison_subsets_for_observables, plot_observables_true_vs_linearized
 from .utils import compute_all_1pt_utilities, compute_max_utilities, convert_max_utilities_to_dataframe, \
     convert_max_utilities_to_flat_dataframe
