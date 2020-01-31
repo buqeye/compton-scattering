64cff876b8b37f5b31a5197d4400e8cb1e5109fa
Jordan Melendez <jordan.melendez@datarobot.com>
2020-01-31 16:53:55 -0500

    New stuff

--- compton/__init__.py
+++ compton/__init__.py
@@ -1,5 +1,5 @@
 from .convergence import ComptonObservable, expansion_parameter, expansion_parameter_transfer_cm, order_transition, \
-    coefficients, RBFJump, create_observable_set
+    coefficients, RBFJump, create_observable_set, posterior_precision_linear, shannon_expected_utility
 from .kinematics import omega_cm_from_lab, omega_outgoing_lab, theta_cm_from_lab, cos0_cm_from_lab, \
     momentum_transfer_cm, momentum_transfer_lab, beta_boost, gamma_boost, dsg_proton_low_energy, sigma3_low_energy
 from .constants import mass_proton, alpha_fine, hbarc, fm2_to_nb, proton_pol_vec_mean, neutron_pol_vec_mean, \
