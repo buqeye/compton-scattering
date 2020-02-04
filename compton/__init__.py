9b74301cc427dd0d5e6d32ae5bc23cd9fc1dbe3a
Jordan Melendez <jordan.melendez@datarobot.com>
2020-02-04 16:53:56 -0500

    Migrated rc params and vmax computations to code

--- compton/__init__.py
+++ compton/__init__.py
@@ -7,6 +7,7 @@ from .constants import mass_proton, alpha_fine, hbarc, fm2_to_nb, proton_pol_vec
     proton_pol_vec_trans_mean, proton_pol_vec_trans_std, neutron_pol_vec_trans_mean, neutron_pol_vec_trans_std, \
     P_trans, pol_vec_trans_tex_names, omega_lab_cusp, observables_unique, observables_name_map, accuracy_levels
 from .graphs import plot_subsets, plot_utilities_all_observables, plot_comparison_subsets_and_truncation, \
-    plot_comparison_subsets_for_observables, plot_observables_true_vs_linearized, offset_scatterplot_data
+    plot_comparison_subsets_for_observables, plot_observables_true_vs_linearized, offset_scatterplot_data, \
+    setup_rc_params, compute_vmax
 from .utils import compute_all_1pt_utilities, compute_max_utilities, convert_max_utilities_to_dataframe, \
     convert_max_utilities_to_flat_dataframe, ComptonExperiment, create_experiment_infos
