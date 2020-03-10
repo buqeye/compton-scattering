9c71800c5b97432851e440c472c7a1b452b23386
Jordan Melendez <jordan.melendez@datarobot.com>
2020-03-09 20:56:54 -0400

    New funcs

--- compton/__init__.py
+++ compton/__init__.py
@@ -1,13 +1,15 @@
 from .convergence import ComptonObservable, expansion_parameter, expansion_parameter_transfer_cm, order_transition, \
-    coefficients, RBFJump, create_observable_set, posterior_precision_linear, shannon_expected_utility
+    coefficients, RBFJump, create_observable_set, posterior_precision_linear, shannon_expected_utility, \
+    compute_expansion_summation_matrix, expansion_parameter_phillips
 from .kinematics import omega_cm_from_lab, omega_outgoing_lab, theta_cm_from_lab, cos0_cm_from_lab, \
     momentum_transfer_cm, momentum_transfer_lab, beta_boost, gamma_boost, dsg_proton_low_energy, sigma3_low_energy
 from .constants import mass_proton, alpha_fine, hbarc, fm2_to_nb, proton_pol_vec_mean, neutron_pol_vec_mean, \
     proton_pol_vec_std, neutron_pol_vec_std, mass_pion, mass_neutron, \
     proton_pol_vec_trans_mean, proton_pol_vec_trans_std, neutron_pol_vec_trans_mean, neutron_pol_vec_trans_std, \
-    P_trans, pol_vec_trans_tex_names, omega_lab_cusp, observables_unique, observables_name_map, accuracy_levels
+    P_trans, pol_vec_trans_tex_names, omega_lab_cusp, observables_unique, observables_name_map, accuracy_levels, \
+    dsg_label, nucleon_names, DesignLabels, mass_delta
 from .graphs import plot_subsets, plot_utilities_all_observables, plot_comparison_subsets_and_truncation, \
     plot_comparison_subsets_for_observables, plot_observables_true_vs_linearized, offset_scatterplot_data, \
-    setup_rc_params, compute_vmax
+    setup_rc_params, compute_vmax, light_mode, dark_mode, plot_fvr_with_offset
 from .utils import compute_all_1pt_utilities, compute_max_utilities, convert_max_utilities_to_dataframe, \
-    convert_max_utilities_to_flat_dataframe, ComptonExperiment, create_experiment_infos
+    convert_max_utilities_to_flat_dataframe, ComptonExperiment, create_experiment_infos, ref_scale
